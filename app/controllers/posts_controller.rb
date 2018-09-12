083ad80dc32f661f168bf3f7af147678d36137e5
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 01:08:44 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,15 @@ class PostsController < ApplicationController
                               include: [ author: { only: [:name]}])
   end
 
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}]) }
+    end
+  end
+
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
